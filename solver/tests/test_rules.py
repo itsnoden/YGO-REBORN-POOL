7ce7a97a1621fe167380e7ed7c15c58c65d791fe
itsnoden <nodenentity@gmail.com>
2026-09-06 20:37:04 -0400

    Certify Reborn rules profile in tests

--- solver/tests/test_rules.py
+++ solver/tests/test_rules.py
@@ -1,15 +1,22 @@
 import unittest
 
-from reborn.rules import EXPERIMENTAL_CURRENT_TCG_FLAGS, get_profile
+from reborn.rules import REBORN_FLAGS, get_profile
 
 
 class RulesProfileTests(unittest.TestCase):
-    def test_existing_engine_flags_are_named_but_not_reborn_certified(self):
-        self.assertEqual(EXPERIMENTAL_CURRENT_TCG_FLAGS, 12885092352)
-        profile = get_profile('experimental_current_tcg')
-        self.assertFalse(profile['certified_for_reborn'])
+    def test_reborn_profile_is_certified(self):
+        self.assertEqual(REBORN_FLAGS, 12885092352)
+        profile = get_profile('reborn')
+        self.assertTrue(profile['certified_for_reborn'])
         self.assertFalse(profile['first_turn_draw'])
         self.assertEqual(profile['opening_hand'], 5)
+        self.assertEqual(profile['starting_lp'], 8000)
+
+    def test_experimental_alias_matches_reborn_for_old_reports(self):
+        alias = get_profile('experimental_current_tcg')
+        reborn = get_profile('reborn')
+        self.assertEqual(alias['flags'], reborn['flags'])
+        self.assertTrue(alias['certified_for_reborn'])
 
     def test_reference_profiles_remain_uncertified(self):
         self.assertTrue(get_profile('mr1_reference')['first_turn_draw'])
