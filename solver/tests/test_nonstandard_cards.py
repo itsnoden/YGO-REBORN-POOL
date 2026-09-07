8dd52e0c7f24af29f28e6b2d03daadfb2c1bb85e
itsnoden <nodenentity@gmail.com>
2026-09-07 07:46:34 -0400

    Test Longnose Blue Mammoth nonstandard overlay

--- solver/tests/test_nonstandard_cards.py
+++ solver/tests/test_nonstandard_cards.py
@@ -25,13 +25,38 @@ class NonstandardCardTests(unittest.TestCase):
         self.assertEqual(row['atk'], 0)
         self.assertEqual(row['def'], 0)
 
-    def test_runtime_card_data_overlay_adds_level_down(self):
+    def test_longnose_blue_mammoth_is_reviewed_video_game_normal_monster(self):
+        cards = load_nonstandard_cards()
+        spec = cards['Longnose Blue Mammoth']
+        self.assertEqual(spec['engine_id'], 25737191)
+        self.assertEqual(spec['status'], 'reviewed_video_game_only_normal_monster')
+        self.assertEqual(spec['placement'], 'main')
+        self.assertEqual(spec['data']['type'], 17)
+        self.assertEqual(spec['data']['level'], 3)
+        self.assertEqual(spec['data']['attribute'], 1)
+        self.assertEqual(spec['data']['race'], 65536)
+        self.assertEqual(spec['data']['atk'], 800)
+        self.assertEqual(spec['data']['def'], 900)
+
+    def test_longnose_engine_row_is_normal_monster_and_distinct_from_great_long_nose(self):
+        rows = nonstandard_engine_rows()
+        row = next(r for r in rows if r['name'] == 'Longnose Blue Mammoth')
+        self.assertEqual(row['id'], 25737191)
+        self.assertEqual(row['type'], 17)
+        self.assertEqual(row['atk'], 800)
+        self.assertEqual(row['def'], 900)
+        self.assertNotEqual(row['id'], 2356994)
+
+    def test_runtime_card_data_overlay_adds_level_down_and_longnose(self):
         data = {1: {'id': 1}}
         names = {1: 'Existing'}
         added = merge_nonstandard_card_data(data, names)
         self.assertIn(100000250, added)
+        self.assertIn(25737191, added)
         self.assertEqual(names[100000250], 'Level Down!')
+        self.assertEqual(names[25737191], 'Longnose Blue Mammoth')
         self.assertEqual(data[100000250]['type'], 2)
+        self.assertEqual(data[25737191]['type'], 17)
 
     def test_runtime_overlay_rejects_id_collision(self):
         data = {100000250: {'id': 100000250}}
